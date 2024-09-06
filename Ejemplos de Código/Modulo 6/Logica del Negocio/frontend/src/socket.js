56595840369304d50a185f270fa9f971c4d8be8e
jflatorreo <jflatorreo@unal.edu.co>
2024-09-06 09:13:57 -0500

    Update socket.js

--- Ejemplos de Código/Modulo 6/Logica del Negocio/frontend/src/socket.js
+++ Ejemplos de Código/Modulo 6/Logica del Negocio/frontend/src/socket.js
@@ -1,7 +1,16 @@
 import io from 'socket.io-client';
 
-const ENDPOINT = 'http://localhost:3001';  // Adjust this if your server is on a different address
+const ENDPOINT = 'https://e62f-186-155-19-171.ngrok-free.app/';  // Adjust this if your server is on a different address
 
-const socket = io(ENDPOINT);
+const socket = io(ENDPOINT, {
+    transports: ['websocket'],
+    upgrade: false,
+    reconnection: true,
+    reconnectionAttempts: 5,
+    timeout: 10000,
+    extraHeaders: {
+        "ngrok-skip-browser-warning": "69420"
+    }
+});
 
 export default socket;
